dd7f6d57dc5ba3a65a680851532a8ae4d41f6633
Josu <josufmc@hotmail.com>
2018-03-11 20:01:34 +0100

    First commit

---
+++ 1.MyFirstApp/1.MyFirstApp/Controllers/ProcessesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1.MyFirstApp.Controllers
+{
+    public class ProcessesController : Controller
+    {
+        // GET: Processes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string List()
+        {
+            return "Processes list:";
+        }
+    }
+}

54af75b7c4f0f47832a6f15e046e65ed592877de
Josu <josufmc@hotmail.com>
2018-03-12 19:00:28 +0100

    Added some routing tests

---
+++ 1.MyFirstApp/2.ControllerMVC/Controllers/ProviderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2.ControllerMVC.Controllers
+{
+    public class ProviderController : Controller
+    {
+        // GET: Provider
+        public string AllProvidersList()
+        {
+            return @"
+                <ul>
+                    <li>A</li>
+                    <li>B</li>
+                    <li>C</li>
+                </ul>
+            ";
+        }
+    }
+}

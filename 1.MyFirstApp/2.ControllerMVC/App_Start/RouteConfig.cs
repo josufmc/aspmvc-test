9778ce7e100ac53737ec16df2a743a47381914e2
Josu <josufmc@hotmail.com>
2018-03-12 06:48:56 +0100

    Second controller test

---
+++ 1.MyFirstApp/2.ControllerMVC/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace _2.ControllerMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Clients",
+                url: "clients/{name}",
+                defaults: new { controller = "Client", action = "Search", name = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ 1.MyFirstApp/2.ControllerMVC/Controllers/ClientController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2.ControllerMVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Search(string name)
+        {
+            var input = Server.HtmlEncode(name);
+            return Content(input);
+            //return View();
+        }
+    }
+}

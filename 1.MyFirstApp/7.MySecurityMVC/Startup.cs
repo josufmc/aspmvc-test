93755f8e9f02790536bc14aaae54593df684d61e
Josu <josufmc@hotmail.com>
2018-03-18 08:50:22 +0100

    Added authentication test

---
+++ 1.MyFirstApp/7.MySecurityMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_7.MySecurityMVC.Startup))]
+namespace _7.MySecurityMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

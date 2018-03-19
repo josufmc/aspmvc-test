2471b08bf782957057194175a6a5aa5806e79ade
Josu <josufmc@hotmail.com>
2018-03-19 07:08:06 +0100

    Added test scaffolding

---
+++ 1.MyFirstApp/8.MyScaffoldingMVC/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _8.MyScaffoldingMVC.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Name { get; set; }
+        [Display(Name = "Created on")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:YYYY-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedOn { get; set; }
+        [Range(18, 75)]
+        public int Age { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        public EmpDBContext()
+        { }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}

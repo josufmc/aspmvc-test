dc25e4168f476bff45a8218b36e39fc1bd6cd08f
Josu <josufmc@hotmail.com>
2018-03-21 06:54:50 +0100

    Added custom data form test

---
+++ 1.MyFirstApp/10.RegisterMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using _10.RegisterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _10.RegisterMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Register obj)
+        {
+            // Comprobamos validación de vista
+            if (ModelState.IsValid)
+            {
+                MyRegisterEntities db = new Models.MyRegisterEntities();
+                db.Registers.Add(obj);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+    }
+}
